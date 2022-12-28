4735cf1b68c0e1cbf055b7d1d6d94793ebd6d81c
Inesh Tandon <i08polymath@gmail.com>
2022-12-28 12:17:46 +0530

    JS UPDATE
    for loop to execute every open button and close button and modal display

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
 "use strict";
 
-const modal = document.querySelector(".modal");
+const modal = document.querySelectorAll(".modal");
 const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnOpenModal = document.querySelector(".show-modal");
+const btnsCloseModal = document.querySelectorAll(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+// const openModal = function () {
+//   modal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// };
+
+const closeModal = function (toBeChanged) {
+  for (let i = 0; i < modal.length; i++) {
+    if (modal[i].dataset.select == toBeChanged) {
+      modal[i].classList.add("hidden");
+      overlay.classList.add("hidden");
+    }
+  }
 };
 
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+const openModal = function (toBeChanged) {
+  // console.log(`did it work ${toBeChanged}`);
+
+  for (let i = 0; i < modal.length; i++) {
+    if (modal[i].dataset.select == toBeChanged) {
+      modal[i].classList.remove("hidden");
+      overlay.classList.remove("hidden");
+    }
+  }
 };
 
-btnCloseModal.addEventListener("click", closeModal);
-btnOpenModal.addEventListener("click", openModal);
+//btnsCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", () => {
+  // going through all the modals are removing from all
+  for (let i = 0; i < modal.length; i++) {
+    modal[i].classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+});
+
+// btnsOpenModal.map(btn => {...}) this wont work becuase btnsopenmodal is a nodeList and not an array
+// use another function inside addEventListener to call a function with param passed in to prevent it from calling
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", () => {
+    openModal(btnsOpenModal[i].dataset.select);
+  });
+}
+
+for (let i = 0; i < btnsCloseModal.length; i++) {
+  btnsCloseModal[i].addEventListener("click", () => {
+    closeModal(btnsOpenModal[i].dataset.select);
+  });
+}
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
